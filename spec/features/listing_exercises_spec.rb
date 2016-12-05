bf0899b3c49671f677be9ee8292f89c9b4053803
Lucas Oliveira <lcastro.oliveira@gmail.com>
2016-12-05 11:55:11 -0200

    Add charts to workouts

---
+++ spec/features/listing_exercises_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.feature 'Listing Exercises' do
+  before do
+    @john = User.create!(email: 'user@example.com', password: 'password', password_confirmation: 'password')
+    login_as(@john)
+
+    @e1 = @john.exercises.create(duration_in_min: 20, workout: 'Body building routine', workout_date: '2015-08-09')
+    @e2 = @john.exercises.create(duration_in_min: 20, workout: 'Body cardio', workout_date: '2015-08-10')
+  end
+
+  scenario 'show user\'s workout for last 7 days' do
+    visit '/'
+    click_link 'My Lounge'
+    expect(page).to have_content(@e1.duration_in_min)
+    expect(page).to have_content(@e1.workout)
+    expect(page).to have_content(@e1.workout_date)
+
+    expect(page).to have_content(@e2.duration_in_min)
+    expect(page).to have_content(@e2.workout)
+    expect(page).to have_content(@e2.workout_date)
+  end
+end
